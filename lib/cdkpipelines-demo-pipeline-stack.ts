0da2f6aca0108408e40aed1d42ba8565b5bd86b3
root <root@ip-172-31-30-164.ap-northeast-1.compute.internal>
2021-09-15 06:05:26 +0000

    find root cause

--- lib/cdkpipelines-demo-pipeline-stack.ts
+++ lib/cdkpipelines-demo-pipeline-stack.ts
@@ -23,7 +23,7 @@ export class CdkpipelinesDemoPipelineStack extends Stack {
          commands: [
            'npm ci',
            'npm run build',
-           'cdk synth'
+           'npx cdk synth'
          ],
        }),
     });
